968b529944ee8ec38449c8d222148b39fe45d697
DeyKast <deykast@users.noreply.github.com>
2023-06-29 17:11:29 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,51 @@
-import Rating from './rating';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <Rating />
-    </div>
-  );
-};
+import FeedbackOptions from './feedbackOptions/feedback-options';
+import Notification from './notification/notification';
+import Section from './section/section';
+import Statistics from './statistics/statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleAddPoint = point => {
+    this.setState(prevState => ({
+      [point]: prevState[point] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = Math.round((100 * good) / total);
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleAddPoint}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
